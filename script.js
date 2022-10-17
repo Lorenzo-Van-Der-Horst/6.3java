9fc5a55093455a93c0797ef6bcc2676cd4dd3879
Lorenzo-Van-Der-Horst <112870039+Lorenzo-Van-Der-Horst@users.noreply.github.com>
2022-10-17 14:52:10 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function ChangeColor(){
+    alert("het werkt");
+    console.log("het werkt");
+}
+
+function Calculator(num, num2) {
+    let answer = num + num2;
+    alert("13");
+    console.log(answer);
+}
+
+function Calculator(num, num2) {
+    let answer = num + num2;
+    console.log(answer);
+    alert(answer);
+}
